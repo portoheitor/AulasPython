179faecc50b9a674bf4c3ff8d0014fd0b2238d9f
Heitor Porto <linuxheitor@hotmail.com>
2024-05-27 22:46:34 -0300

    atualizando dados

--- Aulas/Aulas_exercicios/6_BASE_DADOS/SQLITE/sqlite/main.py
+++ Aulas/Aulas_exercicios/6_BASE_DADOS/SQLITE/sqlite/main.py
@@ -2,24 +2,65 @@ import sqlite3
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
-BD_NAME = 'db.sqlite3'
-BD_FILE = ROOT_DIR / BD_NAME
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
 TABLE_NAME = 'customers'
 
-connection = sqlite3.connect(BD_FILE)
+connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-#CRIAR TABELA
+# CRUD - Create Read   Update Delete
+# SQL -  INSERT SELECT UPDATE DELETE
+
+# Cria a tabela
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
     'id INTEGER PRIMARY KEY AUTOINCREMENT,'
     'name TEXT,'
-    'wight REAL'
+    'weight REAL'
     ')'
 )
+connection.commit()
 
+# Registrar valores nas colunas da tabela
+sql = (
+    f'INSERT INTO {TABLE_NAME} (name, weight)'
+    'VALUES (:nome, :peso)'
+)
+# cursor.execute(sql, ['Joana', 4])
+# cursor.executemany(
+#     sql,
+#     (
+#         ('Joana', 4), ('Luiz', 5)
+#     )
+# )
+
+lista_customers = (
+    {'nome': 'Larissa', 'peso': 3},
+    {'nome': 'Erci', 'peso': 2},
+    {'nome': 'Romanele', 'peso': 4},
+    {'nome': 'Derci', 'peso': 5},
+)
+
+# cursor.execute(sql, {'nome': 'Sem nome', 'peso': 3})
+cursor.executemany(sql, lista_customers)
 connection.commit()
 
 cursor.close()
 connection.close()
+"""
+# CUIDADO: fazendo delete sem where
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+
+# DELETE mais cuidadoso
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+"""
+
+if __name__ == '__main__':
+    ...
